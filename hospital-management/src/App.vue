3b89e7af8a279fc0953d8b9072560a1ee63a621c
kiran-salian <t-kiran.rohithsalian@paypay-corp.co.jp>
2025-02-25 14:32:40 +0530

    initial setup

---
+++ hospital-management/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <h1>Hospital Management System</h1>
+    <div class="container">
+      <div v-for="node of data">
+        <div>{{ node.name }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import json from './data.json'
+const data = ref(json)
+</script>
+<style></style>
